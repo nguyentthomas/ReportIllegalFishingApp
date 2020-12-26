882a253e45a53c498bd3cbe330bcd237cf9bd00b
nguyentthomas <nguyen.thomastri@gmail.com>
2020-12-26 22:17:00 +1100

    Add project files.

---
+++ RIF/RIF/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using RIF.ViewModels;
+using RIF.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace RIF
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
